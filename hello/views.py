025be346e9955309bdb6e2181706a3d2164c9873
ankurpapneja <attheencores@gmail.com>
2017-04-02 07:30:11 -0700

    disable csrf

--- hello/views.py
+++ hello/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Greeting
+from django.views.decorators.csrf import csrf_exempt
+
 import truther
 
+@csrf_exempt
 def index(request):
     return render(request, 'index.html')
 
+@csrf_exempt
 def inputt(request):
     if request.method == 'GET':
         return render(request, 'input.html')
