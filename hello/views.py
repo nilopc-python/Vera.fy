50969ef14cd402de303e3782776aa6158da6e089
ankurpapneja <attheencores@gmail.com>
2017-04-02 09:23:40 -0700

    change response format

--- hello/views.py
+++ hello/views.py
@@ -2,21 +2,23 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Greeting
 from django.views.decorators.csrf import csrf_exempt
-from django.shortcuts import render_to_response
+#from django.shortcuts import render_to_response
 import truther
 
 @csrf_exempt
 def index(request):
     return render(request, 'index.html')
 
-@render_to('hello:index.html')
 @csrf_exempt
 def inputt(request):
     if request.method == 'POST':
         input_data = request.POST.get("input_str")
-        return {
-            "output": truther.truthme(input_data)
-        }
+        vars = dict()
+        vars['output'] = truther.truthme(input_data)
+        return render(request, 'input.html', context=vars, status=200)
+        #return {
+        #    "output": truther.truthme(input_data)
+        #}
     else:
         return render(request, 'input.html')
     
