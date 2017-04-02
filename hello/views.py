eca2818f3bea688a0fc7762e08d38eb045c85dda
ankurpapneja <attheencores@gmail.com>
2017-04-02 10:05:03 -0700

    open webbrowser based on input

--- hello/views.py
+++ hello/views.py
@@ -5,6 +5,7 @@ from .models import Greeting
 from django.views.decorators.csrf import csrf_exempt
 #from django.shortcuts import render_to_response
 import truther
+import webbrowser
 
 @csrf_exempt
 def index(request):
@@ -14,21 +15,16 @@ def index(request):
 def inputt(request):
     if request.method == 'POST':
         input_data = request.POST.get("input_str")
-        #vars = dict()
-        temp_var = "False"
+
         if truther.truthme(input_data):
-            temp_var = "True"
-            #vars['output'] = "True"
-        #else:
-        #    vars['output'] = "False"
-        print >>sys.stderr, temp_var
-        return {
-            "output": str(truther.truthme(input_data))
-        }
-        #return render(request, 'input.html', context=vars, status=200)
-        return {
-            "output": str(truther.truthme(input_data))
-        }
+            webbrowser.open_new("http://google.com")
+        else:
+            webbrowser.open_new("http://bing.com")
+            
+        return render(request, 'input.html')
+        #return {
+        #    "output": str(truther.truthme(input_data))
+        #}
     else:
         return render(request, 'input.html')
     
